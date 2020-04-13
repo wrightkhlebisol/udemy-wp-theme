fe7816f6cded1eed2938da6576820e170d3d3397
Wright <wrightkhlebisol@gmail.com>
2020-04-13 23:07:04 +0100

    Added support for nav-menu, body-class and header/footer templates

--- functions.php
+++ functions.php
@@ -4,9 +4,11 @@
 
 //  Includes
 include_once(get_template_directory() . '/includes/front/enqueue.php');
+include_once(get_template_directory() . '/includes/setup.php');
 
 
 //  Hooks
 add_action('wp_enqueue_scripts', 'wu_enqueue');
+add_action('after_setup_theme', 'wu_setup_theme');
 
 //  Shortcodes
