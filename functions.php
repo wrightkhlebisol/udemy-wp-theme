33ac8ca5007ba42aa92eeba5e4d067bd73ae0602
Wright <wrightkhlebisol@gmail.com>
2020-04-14 00:17:57 +0100

    Created sidebar section

--- functions.php
+++ functions.php
@@ -5,10 +5,12 @@
 //  Includes
 include_once(get_template_directory() . '/includes/front/enqueue.php');
 include_once(get_template_directory() . '/includes/setup.php');
+include_once(get_template_directory() . '/includes/widgets.php');
 
 
 //  Hooks
 add_action('wp_enqueue_scripts', 'wu_enqueue');
 add_action('after_setup_theme', 'wu_setup_theme');
+add_action('widgets_init', 'wu_widgets');
 
 //  Shortcodes
