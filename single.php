d4b480f10f961b84963098192d02e4a75ddcded1
Wright <wrightkhlebisol@gmail.com>
2020-04-15 23:39:21 +0100

    Created single post page and fixed dynamic tags display

--- single.php
+++ single.php
@@ -5,25 +5,6 @@
 <section id="content">
 
     <div class="content-wrap">
-
-        <div class="section header-stick bottommargin-lg clearfix" style="padding: 20px 0;">
-            <div>
-                <div class="container clearfix">
-                    <span class="label label-danger bnews-title">Breaking News:</span>
-
-                    <div class="fslider bnews-slider nobottommargin" data-speed="800" data-pause="6000" data-arrows="false" data-pagi="false">
-                        <div class="flexslider">
-                            <div class="slider-wrap">
-                                <div class="slide"><a href="#"><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </strong></a></div>
-                                <div class="slide"><a href="#"><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </strong></a></div>
-                                <div class="slide"><a href="#"><strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </strong></a></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <div class="container clearfix">
 
             <!-- Post Content
@@ -69,13 +50,6 @@
                     
 
                 </div><!-- #posts end -->
-
-                <!-- Pagination
-            ============================================= -->
-                <ul class="pager nomargin">
-                    <li class="previous"><?php previous_posts_link('&larr; Older') ?></li>
-                    <li class="next"><?php next_posts_link('Newer &rarr;') ?></li>
-                </ul><!-- .pager end -->
 
             </div><!-- .postcontent end -->
 
