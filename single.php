9825972b57a4d8d66e2d1272a2e5155dfb09b25b
Wright <wrightkhlebisol@gmail.com>
2020-04-16 11:19:10 +0100

    Implemented wp_link_pages post break navigation

--- single.php
+++ single.php
@@ -56,37 +56,16 @@
                                     ============================================= -->
                                     <div class="entry-content notopmargin">
 
-                                        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-
-                                        <p>Nullam id dolor id nibh ultricies vehicula ut id elit. <a href="#">Curabitur blandit tempus porttitor</a>. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Vestibulum id ligula porta felis euismod semper.</p>
-
-                                        <blockquote><p>Vestibulum id ligula porta felis euismod semper. Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper.</p></blockquote>
-
-                                        <p>Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cras mattis consectetur purus sit amet fermentum. Donec id elit non mi porta gravida at eget metus.</p>
-
-                                        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Aenean lacinia bibendum nulla sed consectetur. Cras justo odio, dapibus ac facilisis in, egestas eget quam. <a href="#">Nullam quis risus eget urna</a> mollis ornare vel eu leo. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
-
-                                        <pre>
-                                        #header-inner {
-                                            width: 940px;
-                                            margin: 0 auto;
-                                            padding-top: 40px;
-                                        }</pre>
-
-                                        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-
-                                        <p>Nullam id dolor id nibh ultricies vehicula ut id elit. Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Donec id elit non mi porta gravida at eget metus. Vestibulum id ligula porta felis euismod semper.</p>
+                                        <?php 
+                                            the_content();
+                                            wp_link_pages();
+                                        ?>
                                         <!-- Post Single - Content End -->
 
                                         <!-- Tag Cloud
                                         ============================================= -->
                                         <div class="tagcloud clearfix bottommargin">
-                                            <a href="#">general</a>
-                                            <a href="#">information</a>
-                                            <a href="#">media</a>
-                                            <a href="#">press</a>
-                                            <a href="#">gallery</a>
-                                            <a href="#">illustration</a>
+                                            <?php echo get_the_tag_list('', ' '); ?>
                                         </div><!-- .tagcloud end -->
 
                                         <div class="clear"></div>
