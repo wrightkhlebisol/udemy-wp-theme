e06cb1493ceb1667f50379ceeaa47f1e28d796fd
Wright <wrightkhlebisol@gmail.com>
2020-04-21 00:47:27 +0100

    Update default section names to suitably themed names

--- includes/theme-customizer.php
+++ includes/theme-customizer.php
@@ -7,6 +7,11 @@ function wu_customize_register($wp_customize)
         'description'   =>  '<p>Udemy Theme Settings</p>',
         'priority'      =>  160
     ]);
+
     wu_social_customizer_section($wp_customize);
     wu_misc_customizer_section($wp_customize);
+
+    echo '<pre>';
+    var_dump($wp_customize);
+    echo '</pre>';
 }
